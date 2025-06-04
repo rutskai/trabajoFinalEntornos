22c1fb440667004ad336483a429c5fb33622b25b
DrChungo <litingo1000@gmail.com>
2025-06-04 19:38:07 +0200

    Update Conversor.java

--- Conversor.java
+++ Conversor.java
@@ -1,17 +1,4 @@
-package conversion;
 
-public class Conversor {
-
-	
-	private final static double KM_A_MILLAS = 0.62;
-
-	private final static double MB_A_ATM = 0.00098;
-	private final static double KAL_A_JULIOS = 4.184;
-
-	public static double redondear(double num) {
-
-		return Math.round(num * 100) / 100.0;
-	}
 
 	
 
@@ -30,4 +17,3 @@ public class Conversor {
 	
 
 
-}
