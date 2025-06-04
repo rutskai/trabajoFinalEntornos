c4792e7a7cbbaa771d62120ee677664e4797dae8
horabaixa <litingo1000@gmail.com>
2025-06-04 18:52:23 +0200

    Subo Conversor.java de milibares y atmosferas

---
+++ Conversor.java
@@ -0,0 +1,33 @@
+package github;
+
+public class Conversor {
+
+	
+	private final static double KM_A_MILLAS = 0.62;
+
+	private final static double MB_A_ATM = 0.00098;
+	private final static double CAL_A_JULIOS = 4.184;
+
+	public Conversor() {
+	}
+
+//Atmosferas a milibares
+	public static double atmosferas(double milibares) {
+
+		return redondear(milibares * MB_A_ATM) ;
+	}
+	
+//Milibares a Atmosferas
+	public static double milibares(double atmosferas) {
+
+		return redondear( atmosferas / MB_A_ATM) ;
+	}
+
+	
+	
+
+	public static double redondear(double num) {
+
+		return Math.round(num * 100) / 100.0;
+	}
+}
