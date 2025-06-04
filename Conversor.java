dd5b9cb1728cebdb20482de821c0023d255b0194
DrChungo <litingo1000@gmail.com>
2025-06-04 19:26:23 +0200

    Update Conversor.java
    retocando para que algunas cosas esten igual

--- Conversor.java
+++ Conversor.java
@@ -1,4 +1,4 @@
-package github;
+package conversion;
 
 public class Conversor {
 
@@ -6,7 +6,7 @@ public class Conversor {
 	private final static double KM_A_MILLAS = 0.62;
 
 	private final static double MB_A_ATM = 0.00098;
-	private final static double CAL_A_JULIOS = 4.184;
+	private final static double KAL_A_JULIOS = 4.184;
 
 	public Conversor() {
 	}
